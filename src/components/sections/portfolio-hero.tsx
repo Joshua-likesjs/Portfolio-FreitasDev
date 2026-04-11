4dfdfff2333f074447995658928f4941234b6a0f
Joshua-likesc# <galego.jjdocarmo@gmail.com>
2026-04-11 14:28:59 -0400

    image mod

--- src/components/sections/portfolio-hero.tsx
+++ src/components/sections/portfolio-hero.tsx
@@ -176,7 +176,7 @@ const ProfileImage: React.FC<{ isDark: boolean }> = ({ isDark }) => {
 
   return (
     <div
-      className="absolute top-11/12 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10"
+      className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 z-10"
       style={{
         opacity,
         transition: "opacity 1.5s ease-out",

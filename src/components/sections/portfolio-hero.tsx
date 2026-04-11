9b466c38b77dd38c0b836dbf9be3b438a78ef582
Joshua-likesc# <galego.jjdocarmo@gmail.com>
2026-04-10 20:46:28 -0400

    image hero mod

--- src/components/sections/portfolio-hero.tsx
+++ src/components/sections/portfolio-hero.tsx
@@ -179,7 +179,7 @@ const ProfileImage: React.FC<{ isDark: boolean }> = ({ isDark }) => {
   }, []);
 
   // Square sizing so rounded-full makes a circle
-  const size = "w-[80px] h-[180px] sm:w-[110px] sm:h-[110px] md:w-[130px] md:h-[130px] lg:w-[100px] lg:h-[200px]";
+  const size = "w-[80px] h-[180px] sm:w-[110px] sm:h-[110px] md:w-[130px] md:h-[130px] lg:w-[100px] lg:h-[250px]";
 
   return (
     <div
